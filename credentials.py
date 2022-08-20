e220cbc99c06c04822bb0098cfe1d4dc9f6edcb5
Yonatan Berkovitch <yoniberk199@gmail.com>
2022-08-20 15:57:57 +0300

    new

--- credentials.py
+++ credentials.py
@@ -1,2 +1,2 @@
 bot_token = "5772556545:AAHpgaMk8ihcEpD9elKcV17eeVf_Stb49Jo"
-bot_user_name = "Quote4Today"
+bot_chat_id = "-1001611107723"
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import random
+import schedule
 import telegram
 import QuoteData
-from credentials import bot_user_name, bot_token
+import time
+from credentials import bot_chat_id, bot_token
 
-global bot
-global TOKEN
 TOKEN = bot_token
 bot = telegram.Bot(token=TOKEN)
+chat_id = bot_chat_id
+
 
 def send_msg():
     data = QuoteData.load_json_data("t.json")
     quote_number = len(data["quote"])
     random_index = random.randint(0, quote_number)
     msg = data["quote"][random_index] + "\n" + "<b><i>" + data["ref"][random_index] + "</i></b>"
-    bot.send_message(chat_id="-1001611107723", text=msg, parse_mode=telegram.ParseMode.HTML)
+    bot.send_message(chat_id=chat_id, text=msg, parse_mode=telegram.ParseMode.HTML)
 
 
-send_msg()
+schedule.every().day.at("09:00").do(send_msg)
+schedule.every().day.at("12:00").do(send_msg)
+schedule.every().day.at("17:00").do(send_msg)
+schedule.every().day.at("20:00").do(send_msg)
+
+#schedule.every(5).seconds.do(send_msg)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
